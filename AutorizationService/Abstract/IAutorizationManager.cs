b12517ad6119e703a5ef839a3055af35b87533ef
Oleksii Venherets <Oleksii_Venherets@epam.com>
2018-11-09 17:56:29 +0200

    Start added autorization

---
+++ AutorizationService/Abstract/IAutorizationManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using AutorizationService.Models;
+
+namespace AutorizationService.Abstract
+{
+    public interface IAutorizationManager
+    {
+        Task RegisterUser(UserModel userModel);
+        Task LogIn(LoginModel loginModel);
+        Task LogOff();
+    }
+}
---
+++ AutorizationService/Managers/AutorizationManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutorizationService.Abstract;
+using AutorizationService.Models;
+using MentoringProgramApplication.DataLayer.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AutorizationService.Managers
+{
+    public class AutorizationManager : IAutorizationManager
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AutorizationManager(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public async Task RegisterUser(UserModel userModel)
+        {
+            var user = new User();
+            var result = await userManager.CreateAsync(user, userModel.Password);
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, false);
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public async Task LogIn(LoginModel loginModel)
+        {
+            var result = await signInManager.PasswordSignInAsync(loginModel.Email, loginModel.Password, loginModel.RememberMe, false);
+            if (!result.Succeeded)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public async Task LogOff()
+        {
+            await signInManager.SignOutAsync();
+        }
+    }
+}
